01086356e66df1ac7b7f3de8202106da659ceb4b
tathagatalaskar <tathagata.laskar24@gmail.com>
2026-03-14 05:10:22 +0530

    Initialize React application in index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+/* CyberDost v3.0 © 2026 Tathagata Laskar & Devaansh Singh | KRIWA-26-1102 */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><App /></React.StrictMode>);
